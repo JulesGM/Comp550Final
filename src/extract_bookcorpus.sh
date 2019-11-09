4da942f6b753bc1c102361e1d194986ebd7b968f
im-ant <anthony.gx.chen@gmail.com>
2019-11-09 12:38:31 -0500

    script for extracting bookcorpus books onto the server (mostly complete)

--- src/extract_bookcorpus.sh
+++ src/extract_bookcorpus.sh
@@ -1,15 +1,47 @@
-###
+# ============================================================
+# Extracts the bookcorpus on the Mila cluster
+#
+# Usage (after logging into an allocated node with $SLURM_TMPDIR
+# as the temporary directory of that node):
+#   bash extract_bookcorpus.sh
+# ============================================================
+
+# Environmental variables
+VENV_PATH="$SLURM_TMPDIR/cur_venv"                                  # path of vitual environment
+BOOKCORPUS_PATH="$SLURM_TMPDIR/bookcorpus"                          # path to the bookcorpus repository
+BOOKS_DIR="/network/tmp1/chenant/class/comp-550/bookcorpus-books"   # path to the output books
+
+
+# Load module
+module load python/3.7
+
+
+# Create temporary virtual environment and activate
+if [ ! -f "$VENV_PATH" ] ; then
+  virtualenv $VENV_PATH
+fi
+source $VENV_PATH/bin/activate
+
 
 # Clone the bookcorpus repository
-mkdir bookcorpus-repo
-git clone https://github.com/soskek/bookcorpus bookcorpus-repo
+if [ ! -f "$BOOKCORPUS_PATH" ] ; then
+  mkdir $BOOKCORPUS_PATH
+fi
+git clone https://github.com/soskek/bookcorpus $BOOKCORPUS_PATH
+
 
 # Install requirements for bookcorpus
-python -m pip install -r bookcorpus-repo/requirements.txt
+python -m pip install -r $BOOKCORPUS_PATH/requirements.txt
 
-# Get the list of book json
-python -u tmp-bookcorpus/download_list.py > url_list.jsonl
+# Get the list of book json (skipping because they already provide a list)
+#python -u $BOOKCORPUS_PATH/download_list.py > $BOOKCORPUS_PATH/url_list.jsonl
 
+# Get the book
+python $BOOKCORPUS_PATH/download_files.py --list $BOOKCORPUS_PATH/url_list.jsonl --out $BOOKCORPUS_PATH/out_txts --trash-bad-count
+
+
+# Copy the books to an appropriate directory (incomplete)
+# cp -r $BOOKCORPUS_PATH/out_txts $BOOKS_DIR
 
 
 
