cb26b257295cb32c91e65188047a775f65c31553
Jules Gagnon-Marchand <jgagnonmarchand@gmail.com>
2019-12-09 01:20:55 -0500

    common_sense_qa run sh script

---
+++ src/finetuning/run_run_commonsense_qa.sh
@@ -0,0 +1,79 @@
+set -e
+set -u
+
+VENV_PATH="$SLURM_TMPDIR/cur_venv"
+COMMONSENSEQA="$SLURM_TMPDIR/commonsenseqa"
+BERT_URL="https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip"
+BERT_FILE_NAME=$(python3 -c "print(\"$BERT_URL\".split(\"/\")[-1].split(\".\")[0])")
+echo $BERT_FILE_NAME
+BERT_DIR="$SLURM_TMPDIR/bert_model"
+
+if [ ! -d "$COMMONSENSEQA" ] ; then
+  mkdir "$COMMONSENSEQA"
+fi
+
+# Download the dataset
+cp train_rand_split.jsonl "$COMMONSENSEQA"/train_rand_split.jsonl
+cp dev_rand_split.jsonl "$COMMONSENSEQA"/dev_rand_split.jsonl
+cp test_rand_split.jsonl "$COMMONSENSEQA"/test_rand_split.jsonl
+
+
+echo -e "\n###############################################################"
+echo "# Load tensorflow gpu"
+echo "###############################################################"
+# Load python
+module load python/3.7
+
+echo -e "\n###############################################################"
+echo "# Activate VENV"
+echo "###############################################################"
+#rm -rf "$VENV_PATH" || true
+# Set up and activate temporary virtualenv
+if [ ! -d "$VENV_PATH" ] ; then
+  virtualenv "$VENV_PATH"
+fi
+source "$VENV_PATH/bin/activate"
+
+echo -e "\n###############################################################"
+echo "# Load CUDA and CUDNN "
+echo "###############################################################"
+module load cuda/10.0
+module load cuda/10.0/cudnn/7.3
+python -m pip install tensorflow-gpu==1.15.0rc2 tqdm -q
+
+echo -e "\n###############################################################"
+echo "# Maybe Download the BERT model"
+echo "###############################################################"
+if [ ! -d "$BERT_DIR" ] ; then
+  mkdir "$BERT_DIR"
+fi
+
+if [ ! -f "$BERT_DIR/$BERT_FILE_NAME.zip" ] ; then
+  wget "$BERT_URL" -O "$BERT_DIR/$BERT_FILE_NAME.zip"
+  unzip "$BERT_DIR/$BERT_FILE_NAME.zip" -d "$BERT_DIR/"
+  mv "$BERT_DIR/$BERT_FILE_NAME"/* "$BERT_DIR"
+fi
+
+if [ ! -d "$COMMONSENSEQA/checkpoints" ] ; then
+  mkdir "$COMMONSENSEQA/checkpoints"
+fi
+
+echo -e "\n###############################################################"
+echo "# Running run_commonsense_qa.py"
+echo "###############################################################"
+
+python run_commonsense_qa.py \
+  --data_dir="$COMMONSENSEQA"\
+  --split=rand \
+  --bert_config_file="$BERT_DIR/bert_config.json" \
+  --vocab_file="$BERT_DIR/vocab.txt" \
+  --output_dir="$COMMONSENSEQA/checkpoints" \
+  --init_checkpoint="$BERT_DIR/bert_model.ckpt" \
+  --do_lower_case=False \
+  --max_seq_length=128 \
+  --do_train=True \
+  --do_eval=True \
+  --do_predict=False \
+  --train_batch_size=8 \
+  --eval_batch_size=8 \
+  --predict_batch_size=8 \
